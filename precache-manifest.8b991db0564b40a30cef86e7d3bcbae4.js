e8e87f7c0fcdae807f899e5221dbefe462f43a09
Ahmed Khaled <47076615+ahmedswaby@users.noreply.github.com>
2019-01-27 18:14:17 +0200

    Add files via upload

---
+++ precache-manifest.8b991db0564b40a30cef86e7d3bcbae4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eac843c249df00468bb5",
+    "url": "/crud-react-app/static/js/runtime~main.eac843c2.js"
+  },
+  {
+    "revision": "03cb404d32bc339bdd05",
+    "url": "/crud-react-app/static/js/main.03cb404d.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/crud-react-app/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "03cb404d32bc339bdd05",
+    "url": "/crud-react-app/static/css/main.8e88cff5.chunk.css"
+  },
+  {
+    "revision": "74f5fbda9213902a6ad82a5d8f3a71b9",
+    "url": "/crud-react-app/index.html"
+  }
+];
